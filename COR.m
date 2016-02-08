dca74c6e3513b5923fa137954aa90451dd1c98f0
Jingjing Yang <yjingj@gmail.com>
2016-02-08 18:11:47 -0500

    revise toolbox

--- COR.m
+++ COR.m
@@ -1,5 +1,6 @@
 function R = COR(COV)
-   
+% function to generate correlation matrix from the covariance matrix
+
 R = COV ./ (sqrt(diag(COV)) * sqrt(diag(COV))' );
 
 end
--- runjob.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/csh
-nohup matlab -nodisplay < obese_study.m > outputJune22.txt &
